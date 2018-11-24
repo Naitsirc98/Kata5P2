bdeb7322916c3ff9259cde61d64d579dcf37ba6c
Cristian Herrera <naitsirc98@gmail.com>
2018-11-24 16:40:51 +0000

    Version 1

---
+++ Kata5P2/src/kata4/main/Kata4.java
@@ -0,0 +1,53 @@
+package kata4.main;
+
+import java.util.List;
+
+import kata4.model.Histogram;
+import kata4.model.Mail;
+import kata4.view.HistogramDisplay;
+import kata4.view.MailHistogramBuilder;
+import kata4.view.MailListReader;
+
+public class Kata4 {
+	
+	public static void main(String[] args) {
+		
+		Kata4 kata = new Kata4("email.txt");
+		
+		kata.execute();
+
+	}
+	
+	private final String filename;
+	private List<Mail> mails;
+	private Histogram<String> histogram;
+	private HistogramDisplay display;
+
+	public Kata4(final String filename) {
+		this.filename = filename;
+	}
+	
+	private void execute() {
+		
+		input();
+		
+		process();
+		
+		output();
+		
+	}
+	
+	private void input() {
+		mails = MailListReader.read(filename);
+	}
+	
+	private void process() {
+		histogram = MailHistogramBuilder.build(mails);
+	}
+	
+	private void output() {
+		display = new HistogramDisplay(histogram);
+		display.execute();
+	}
+
+}
